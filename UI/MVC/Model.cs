b0240fac36831680ca9c30bc413b7563e45db386
LiuHan <649547021@qq.com>
2022-12-15 12:44:03 +0800

    更新一下目录结构

--- UI/MVC/Model.cs
+++ UI/MVC/Model.cs
@@ -1,39 +1,37 @@
 using System;
 using UnityEngine;
 
-namespace RGScript.UI.MVC {
-    public abstract class Model<T> : MonoBehaviour {
-        private Controller _controller;
+public abstract class Model<T> : MonoBehaviour {
+    private Controller _controller;
         
-        [SerializeField]
-        protected T data;
+    [SerializeField]
+    protected T data;
         
-        protected abstract void OnShow(ValueType extraParams);
-        protected abstract void OnHide(ValueType extraParams);
-        protected virtual void OnRefresh() {}
-        protected abstract void ProcessMessage(Message message);
+    protected abstract void OnShow(ValueType extraParams);
+    protected abstract void OnHide(ValueType extraParams);
+    protected virtual void OnRefresh() {}
+    protected abstract void ProcessMessage(Message message);
         
-        private void Start() {
-            _controller = transform.GetComponent<Controller>();
+    private void Start() {
+        _controller = transform.GetComponent<Controller>();
+    }
+        
+    public T HandleMessage(Message message) {
+        switch (message.Command) {
+            case Message.CommonCommand.Show:
+                OnShow(message.ExtraParams);
+                break;
+            case Message.CommonCommand.Hide:
+                OnHide(message.ExtraParams);
+                break;
+            case Message.CommonCommand.Refresh:
+                OnRefresh();
+                break;
+            default:
+                ProcessMessage(message);
+                break;
         }
-        
-        public T HandleMessage(Message message) {
-            switch (message.Command) {
-                case Message.CommonCommand.Show:
-                    OnShow(message.ExtraParams);
-                    break;
-                case Message.CommonCommand.Hide:
-                    OnHide(message.ExtraParams);
-                    break;
-                case Message.CommonCommand.Refresh:
-                    OnRefresh();
-                    break;
-                default:
-                    ProcessMessage(message);
-                    break;
-            }
 
-            return data;
-        }
+        return data;
     }
 }
--- UI/MVC/View.cs
+++ UI/MVC/View.cs
@@ -1,46 +1,42 @@
 using System;
 using UnityEngine;
 
-namespace RGScript.UI.MVC {
-    public abstract class View<T> : MonoBehaviour {
-        private Controller _controller;
-        // ReSharper disable once UnusedMember.Global
-        protected bool IsShowing => _controller.IsShowing;
+public abstract class View<T> : MonoBehaviour {
+    private Controller _controller;
+    protected bool IsShowing => _controller.IsShowing;
 
-        protected abstract void OnShow(T data);
-        protected abstract void OnHide(T data);
-        protected abstract void OnRefresh(T data);
-        protected abstract void ProcessMessage(ValueType command, T data);
-        protected abstract bool OnBackButtonClicked();
+    protected abstract void OnShow(T data);
+    protected abstract void OnHide(T data);
+    protected abstract void OnRefresh(T data);
+    protected abstract void ProcessMessage(ValueType command, T data);
+    protected abstract bool OnBackButtonClicked();
 
-        protected virtual void Start() {
-            _controller = transform.GetComponent<Controller>();
+    protected virtual void Start() {
+        _controller = transform.GetComponent<Controller>();
+    }
+
+    public void HandleCommand(ValueType command, T data) {
+        switch (command) {
+            case Message.CommonCommand.Show:
+                OnShow(data);
+                break;
+            case Message.CommonCommand.Hide:
+                OnHide(data);
+                break;
+            case Message.CommonCommand.Refresh:
+                OnRefresh(data);
+                break;
+            default:
+                ProcessMessage(command, data);
+                break;
         }
+    }
 
-        // ReSharper disable once UnusedMember.Global
-        public void HandleCommand(ValueType command, T data) {
-            switch (command) {
-                case Message.CommonCommand.Show:
-                    OnShow(data);
-                    break;
-                case Message.CommonCommand.Hide:
-                    OnHide(data);
-                    break;
-                case Message.CommonCommand.Refresh:
-                    OnRefresh(data);
-                    break;
-                default:
-                    ProcessMessage(command, data);
-                    break;
-            }
-        }
-
-        protected void DispatchMessage(Message message) {
-            _controller.DispatchMessage(message);
-        }
+    protected void DispatchMessage(Message message) {
+        _controller.DispatchMessage(message);
+    }
         
-        protected void DispatchMessageDelayed(Message message, float time) {
-            _controller.DispatchMessageDelayed(message, time);
-        }
+    protected void DispatchMessageDelayed(Message message, float time) {
+        _controller.DispatchMessageDelayed(message, time);
     }
 }
